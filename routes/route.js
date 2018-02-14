ae376e577498941f8ce04eff056dc31493709f42
Parthiban Rajendran <parthi2929@gmail.com>
2018-02-14 23:28:13 +0530

    ohlc 5 min retrieval almost fixed, some times few empty array may appear

--- routes/route.js
+++ routes/route.js
@@ -42,54 +42,81 @@ exports.ohlc = function (request, response)
         }
     );
 }
-
+//var ohlcModelWrite = mongoose.model('ohlcModelWrite');
 exports.ohlc5 = function (request, response)
 { 
     console.log('Request for ohlc 5 min interval');
     //retrieve entire latest data - 
-    ohlcModelRead.aggregate([
-        {"$project":{
-          "data":{
-            "$let":{
-              "vars":{
-                "mints":{"$arrayElemAt":[{"$arrayElemAt":["$data",0]},0]},
-                "maxts":{"$arrayElemAt":[{"$arrayElemAt":["$data",-1]},0]}
-              },
-              "in":{
-                "$map":{
-                  "input":{"$range":["$$mints",{"$add":["$$maxts",300]},300]},
-                  "as":"rge",
-                  "in":{
-                    "$let":{
-                      "vars":{
-                        "five":{
-                          "$filter":{
-                            "input":"$data",
-                            "as":"fres",
-                            "cond":{
-                              "$and":[
-                                {"$gte":[{"$arrayElemAt":["$$fres",0]},"$$rge"]},
-                                {"$lt":[{"$arrayElemAt":["$$fres",0]},{"$add":["$$rge",300]}]}
-                              ]
+    ohlcModelRead.aggregate(
+      [
+        {"$project":
+          {
+            "data":
+            {
+              "$let":
+              {
+                "vars":
+                {
+                  "mints":{"$arrayElemAt":[{"$arrayElemAt":["$data",0]},0]},
+                  "maxts":{"$arrayElemAt":[{"$arrayElemAt":["$data",-1]},0]}
+                },
+                "in":
+                {
+                    "$setDifference":
+                    [
+                        {
+                          "$map":
+                          {
+                            "input":{"$range":["$$mints",{"$add":["$$maxts",300]},300]},
+                            "as":"rge",
+                            "in":
+                            {
+                              "$let":
+                              {
+                                "vars":
+                                {
+                                  "five":
+                                  {
+                                    "$filter":
+                                    {
+                                      "input":"$data",
+                                      "as":"fres",
+                                      "cond":
+                                      {
+                                        "$and":
+                                        [
+                                          {"$gte":[{"$arrayElemAt":["$$fres",0]},"$$rge"]},
+                                          {"$lt":[{"$arrayElemAt":["$$fres",0]},{"$add":["$$rge",300]}]}
+                                        ]
+                                      }
+                                    }
+                                  }
+                                },
+                                "in":
+                                {
+                                    "$cond":[
+                                        {"$eq":["$$five",[]]},
+                                        "$$five",
+                                        [
+                                          {"$arrayElemAt": [{"$arrayElemAt":["$$five",-1]},0]},
+                                          {"$arrayElemAt":[{"$arrayElemAt":["$$five",0]},1]},
+                                          {"$max":{"$map":{"input":"$$five","as":"res","in":{"$arrayElemAt":["$$res",2]}}}},
+                                          {"$min":{"$map":{"input":"$$five","as":"res","in":{"$arrayElemAt":["$$res",3]}}}},
+                                          {"$arrayElemAt":[{"$arrayElemAt":["$$five",-1]},-2]},
+                                          {"$arrayElemAt":[{"$arrayElemAt":["$$five",-1]},-1]}
+                                        ]
+                                    ]
+                                 }
+                              }
                             }
                           }
-                        }
-                      },
-                      "in":[
-                        {"$arrayElemAt":[{"$arrayElemAt":["$$five",-1]},0]},
-                        {"$arrayElemAt":[{"$arrayElemAt":["$$five",0]},1]},
-                        {"$max":{"$map":{"input":"$$five","as":"res","in":{"$arrayElemAt":["$$res",2]}}}},
-                        {"$min":{"$map":{"input":"$$five","as":"res","in":{"$arrayElemAt":["$$res",3]}}}},
-                        {"$arrayElemAt":[{"$arrayElemAt":["$$five",-1]},-2]},
-                        {"$arrayElemAt":[{"$arrayElemAt":["$$five",-1]},-1]}
-                      ]
-                    }
+                      },[]
+                     ]
                   }
                 }
               }
             }
           }
-        }}
       ]
      ,        
         function(error, result)
@@ -104,8 +131,9 @@ exports.ohlc5 = function (request, response)
                 //response.jsonp(JSON.parse(JSON.stringify(result.data)));
                 if (result != null)
                 {
-                    console.log('Request for ohlc 5 min interval successful: '  + result.data);
-                    response.jsonp(result.data);
+                    result = JSON.stringify(result)
+                    console.log('Request for ohlc 5 min interval successful: '  + JSON.parse(result)[0].data);
+                    response.jsonp(JSON.parse(result)[0].data);
                 }                
             }
         }
